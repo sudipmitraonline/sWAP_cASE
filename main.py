2fb4ef8bf4f1ad4d3d93e25459ab791eee497dd6
Sudip Mitra <44464604+sudipmitraonline@users.noreply.github.com>
2019-11-21 22:36:00 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#@Author : Sudip Mitra
+#Email : sudipmitraonline@gmail.com
+
+def swap_case(s):
+    a = ""
+    for let in s:
+        if let.isupper() == True:
+            a+=(let.lower())
+        else:
+            a+=(let.upper())
+    return a
+    
+if __name__ == '__main__':
+    s = input("Enter the string :")
+    result = swap_case(s)
+    print("You Entered : {0}".format(s))
+    print("Revised String : ",result)
